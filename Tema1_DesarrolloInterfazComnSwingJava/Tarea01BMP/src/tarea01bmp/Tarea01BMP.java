a96d5359792865ab3385a8fdf2a78cf8054e556e
Benjamin Mulero Pedrosa <bemupe@hotmail.com>
2022-06-28 17:33:48 +0200

    Add files via upload

---
+++ Tema1_DesarrolloInterfazComnSwingJava/Tarea01BMP/src/tarea01bmp/Tarea01BMP.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea01bmp;
+
+import Formularios.FormularioPrincipal;
+
+/**
+ *
+ * @author benmu
+ */
+public class Tarea01BMP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormularioPrincipal P = new FormularioPrincipal();
+        P.setVisible(true);
+    }
+    
+}
